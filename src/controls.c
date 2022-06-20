db81bd1ddd02d0d0a8ad5a97fc529511f1ef06cb
dma-neves <david.m.neves@live.com>
2022-06-21 00:55:34 +0100

    Started implementing joypad

--- src/controls.c
+++ src/controls.c
@@ -14,30 +14,45 @@ uint8_t keys[8] = {0};
 #define START 6
 #define SELECT 7
 
-static void set_key_value(SDL_Keycode key, uint8_t value)
+static int get_key_index(SDL_Keycode key)
 {
     switch (key)
     {
-        case SDLK_w: keys[UP] = value; break;
-        case SDLK_a: keys[LEFT] = value; break;
-        case SDLK_s: keys[DOWN] = value; break;
-        case SDLK_d: keys[RIGHT] = value; break;
-        case SDLK_j: keys[A] = value; break;
-        case SDLK_k: keys[B] = value; break;
-        case SDLK_n: keys[START] = value; break;
-        case SDLK_m: keys[SELECT] = value; break;
+        case SDLK_w: return UP;
+        case SDLK_a: return LEFT;
+        case SDLK_s: return DOWN;
+        case SDLK_d: return RIGHT;
+        case SDLK_j: return A;
+        case SDLK_k: return B;
+        case SDLK_n: return START;
+        case SDLK_m: return SELECT;
         default: break;    
     }
+
+    return -1;
 }
 
 void controls_pressed(SDL_Keycode key)
 {
-    set_key_value(key, 1);
+    int ki = get_key_index(key);
+    if(ki != -1)
+    {
+        uint8_t action = (((*joyp) >> 0x5) & 0x1) == 0x0;
+        uint8_t direction = (((*joyp) >> 0x4) & 0x1) == 0x0;
+
+        if( (action && ki >= A) || (direction && ki <= RIGHT) )
+            ; // Interrupt
+
+
+        keys[get_key_index(key)] = 1;
+    }
 }
 
 void controls_released(SDL_Keycode key)
 {
-    set_key_value(key, 0);
+    int ki = get_key_index(key);
+    if(ki != -1)
+        keys[get_key_index(key)] = 0;
 }
 
 static void set_joyp_bit(uint8_t bitn, uint8_t value)
@@ -50,8 +65,6 @@ static void set_joyp_bit(uint8_t bitn, uint8_t value)
 
 void write_joyp_reg(uint8_t value)
 {
-    //(*joyp) &= 0x30;
-
     uint8_t action = (((*joyp) >> 0x5) & 0x1) == 0x0;
     uint8_t direction = (((*joyp) >> 0x4) & 0x1) == 0x0;
 
--- src/controls.h
+++ src/controls.h
@@ -2,10 +2,12 @@
 #define CONTROL
 
 #include <SDL2/SDL.h>
+#include <stdint.h>
 
 void controls_pressed(SDL_Keycode key);
 void controls_released(SDL_Keycode key);
 
+uint8_t joyp_interrupt();
 void write_joyp_reg(uint8_t value);
 
 #endif
